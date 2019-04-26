db10161369df55451cc97ec25e779b4f76c94a60
William Pembleton <william.pembleton17@ncf.edu>
2019-04-25 20:17:48 -0400

    Dory works, no texture

--- app/src/main/java/edu/ncf/ar/araquarium/AugmentedImageNode.java
+++ app/src/main/java/edu/ncf/ar/araquarium/AugmentedImageNode.java
@@ -52,7 +52,7 @@ public class AugmentedImageNode extends AnchorNode {
     if (ulCorner == null) {
       ulCorner =
           ModelRenderable.builder()
-              .setSource(context, Uri.parse("Dolphin.sfb"))
+              .setSource(context, Uri.parse("Dory.sfb"))
               .build();
       urCorner =
           ModelRenderable.builder()
@@ -102,7 +102,7 @@ public class AugmentedImageNode extends AnchorNode {
     cornerNode = new Node();
     cornerNode.setParent(this);
     cornerNode.setLocalPosition(localPosition);
-    cornerNode.setLocalRotation(Quaternion.eulerAngles(new Vector3(90,180,0)));
+    cornerNode.setLocalRotation(Quaternion.eulerAngles(new Vector3(180,0,180)));
     cornerNode.setRenderable(ulCorner.getNow(null));
 
     // Upper right corner.
