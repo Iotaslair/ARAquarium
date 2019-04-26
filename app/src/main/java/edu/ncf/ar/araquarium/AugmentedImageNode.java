19c056949315a9e415323658e65967a569314adf
William Pembleton <william.pembleton17@ncf.edu>
2019-04-25 20:10:41 -0400

    Good news: Dolphin Bad news: Dolphin is metal

--- app/src/main/java/edu/ncf/ar/araquarium/AugmentedImageNode.java
+++ app/src/main/java/edu/ncf/ar/araquarium/AugmentedImageNode.java
@@ -52,7 +52,7 @@ public class AugmentedImageNode extends AnchorNode {
     if (ulCorner == null) {
       ulCorner =
           ModelRenderable.builder()
-              .setSource(context, Uri.parse("Crab.sfb"))
+              .setSource(context, Uri.parse("Dolphin.sfb"))
               .build();
       urCorner =
           ModelRenderable.builder()
@@ -102,7 +102,7 @@ public class AugmentedImageNode extends AnchorNode {
     cornerNode = new Node();
     cornerNode.setParent(this);
     cornerNode.setLocalPosition(localPosition);
-    cornerNode.setLocalRotation(Quaternion.eulerAngles(new Vector3(0,0,0)));
+    cornerNode.setLocalRotation(Quaternion.eulerAngles(new Vector3(90,180,0)));
     cornerNode.setRenderable(ulCorner.getNow(null));
 
     // Upper right corner.
